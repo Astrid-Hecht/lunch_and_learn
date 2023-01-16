0a037a6b18f62e09915d0e6b87884d744b2ffe8e
Astrid Hecht <silentaviary@gmail.com>
2023-01-16 14:54:11 -0700

    FIX: Fix typo, recipe consumption fully functional

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,8 +1,8 @@
-class EdamamFacade
+class RecipeFacade
   def self.recipe_search(query)
     target_fields = %i[title image url country]
 
-    results = EdamamService.recipe_search(query)[:hits]
+    results = RecipeService.recipe_search(query)[:hits]
 
     results.map! do |raw_hit|
       raw_hit[:recipe]
