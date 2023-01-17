12a95ed6585bd079ef0d2b9b6cf79ad74a022d79
Astrid Hecht <silentaviary@gmail.com>
2023-01-17 12:06:13 -0700

    FEAT: Write unsplash facade and service

--- app/facades/youtube_facade.rb
+++ app/facades/youtube_facade.rb
@@ -2,7 +2,7 @@ class YoutubeFacade
   def self.mr_history_search(query)
     mr_hist_channel_id = 'UCluQ5yInbeAkkeCndNnUhpw'
 
-    result = YoutbeService.channel_vids_search(query, mr_hist_channel_id)[:items].first
+    result = YoutubeService.channel_vids_search(query, mr_hist_channel_id)[:items].first
 
     { title: result[:snippet][:title], youtube_video_id: result[:id][:videoId] }
   end
