7ea47ee8a3cb0888d840fdfeb35f808df903b4e8
Astrid Hecht <silentaviary@gmail.com>
2023-01-17 11:51:45 -0700

    TEST: Write unsplash service spec

---
+++ spec/services/unsplash_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UnsplashService do
+  let(:photo_search) { UnsplashService.photo_search("China") }
+
+  it 'can search photos', :vcr do
+    expect(channel_search).to be_a(Hash)
+    expect(channel_search[:results]).to be_a(Array)
+
+    first_hit = channel_search[:results].first
+    expect(first_hit).to be_a(Hash)
+
+    expect(first_hit[:alt_description]).to be_a String
+    expect(first_hit[:urls][:full]).to be_a String
+  end
+end
