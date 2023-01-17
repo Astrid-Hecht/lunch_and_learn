dc9617ba57e11db83759aa452789ce4b58e0e097
Astrid Hecht <silentaviary@gmail.com>
2023-01-17 11:34:09 -0700

    FEAT: Write Youtube facade

---
+++ app/facades/youtube_facade.rb
@@ -0,0 +1,9 @@
+class YoutubeFacade
+  def self.mr_history_search(query)
+    mr_hist_channel_id = 'UCluQ5yInbeAkkeCndNnUhpw'
+
+    result = YoutbeService.channel_vids_search(query, mr_hist_channel_id)[:items].first
+
+    { title: result[:snippet][:title], youtube_video_id: result[:id][:videoId] }
+  end
+end
