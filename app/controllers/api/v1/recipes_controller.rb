ff0ba38c66ae8e814c3a98e7d3f399c37c5cbc6a
Astrid Hecht <silentaviary@gmail.com>
2023-01-17 10:31:47 -0700

    TEST: Add sad path testing for recipe search by country

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -7,14 +7,14 @@ class Api::V1::RecipesController < ApplicationController
 
   def search_checker(country)
     country_list = CountryFacade.list_countries
-    country ||= country_list.sample(1)
+    country ||= country_list.sample(1).first
 
     if !country_list.include?(country.downcase)
-      { error: { status: 404, msg: 'placeholder error until error handling is set up' } }
+      {data: { error: { status: 404, msg: 'placeholder error until error handling is set up' } }}
     elsif (results = RecipeFacade.recipe_search(country))
       RecipeSerializer.new(results)
     else
-      []
+      { 'data': [] }
     end
   end
 end
--- spec/requests/api/v1/recipe_request_spec.rb
+++ spec/requests/api/v1/recipe_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
   describe 'Search by Country API endpoint' do
-    context 'returns an array of results' do
+    context 'valid search country renders an array of results' do
       it 'with correctly serialized data', :vcr do
         get '/api/v1/recipes?country=Mexico'
 
@@ -38,6 +38,71 @@ RSpec.describe 'Recipes', type: :request do
 
         expect(recipe[:attributes]).to have_key :image
         expect(recipe[:attributes][:image]).to be_a(String)
+      end
+
+      it 'without extra data', :vcr do
+        get '/api/v1/recipes?country=Mexico'
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response.keys).to eq([:data])
+
+        recipe = parsed_response[:data].first
+
+        expect(recipe.keys).to eq(%i[id type attributes])
+
+        expect(recipe[:attributes].keys).to eq(%i[title url country image])
+      end
+    end
+
+    context 'no search term randomly generates country' do
+      it 'and renders valid results with no extra data' do
+        get '/api/v1/recipes'
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(200)
+
+        expect(parsed_response.keys).to eq([:data])
+        expect(parsed_response[:data]).to be_an Array
+
+        recipe = parsed_response[:data].first
+
+        expect(recipe.keys).to eq(%i[id type attributes])
+
+        expect(recipe[:attributes].keys).to eq(%i[title url country image])
+
+        expect(recipe[:id]).to be_nil
+        expect(recipe[:type]).to eq('recipe')
+        expect(recipe[:attributes]).to be_a Hash
+        expect(recipe[:attributes][:title]).to be_a(String)
+        expect(recipe[:attributes][:url]).to be_a(String)
+        expect(recipe[:attributes][:country]).to be_a(String)
+        expect(recipe[:attributes][:image]).to be_a(String)
+      end
+    end
+
+    context 'renders empty array' do
+      it 'if country is invalid' do
+        get '/api/v1/recipes?country=Globglorbgorb'
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(200)
+
+        expect(parsed_response.keys).to eq([:data])
+        expect(parsed_response[:data].keys).to eq([:error])
+      end
+
+      it 'if country has no recipes' do
+        get '/api/v1/recipes?country=Namibia'
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(200)
+
+        expect(parsed_response.keys).to eq([:data])
+        expect(parsed_response[:data]).to eq([])
       end
     end
   end
