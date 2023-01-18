f531aab1110cf86db15abfc1fe3740d50e0634d8
Astrid Hecht <silentaviary@gmail.com>
2023-01-17 17:06:23 -0700

    TEST: Finish favorites sad path tests

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -27,5 +27,54 @@ RSpec.describe 'Favorites', type: :request do
         expect(body[:success]).to eq('Favorite added successfully')
       end
     end
+
+    context '- sad path -' do
+      it 'wont create a new favorite when fields are missing' do
+        user = create(:user)
+        fav_params = { api_key: user.api_key, country: 'Sealand', recipe_link: 'www.link.com',
+                       recipe_title: 'Recipe No 12121' }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params.delete(:country))
+        post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params.delete(:recipe_link))
+        post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params.delete(:recipe_title))
+
+        created_fav = Favorite.last
+
+        # expect(response).to be_successful
+        # expect(response.status).to eq(201)
+
+        expect(created_fav).to eq(nil)
+        expect(Favorite.all.count).to eq(0)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body).to be_a Hash
+        expect(body.keys).to eq([:data])
+        expect(body[:data].keys).to eq([:error])
+      end
+      it 'wont create a new favorite when no matching api key' do
+        user = create(:user)
+        fav_params = { api_key: 'arbitrarystring', country: 'Sealand', recipe_link: 'www.link.com',
+                       recipe_title: 'Recipe No 12121' }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params)
+
+        created_fav = Favorite.last
+
+        # expect(response).to be_successful
+        # expect(response.status).to eq(201)
+
+        expect(created_fav).to eq(nil)
+        expect(Favorite.all.count).to eq(0)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body).to be_a Hash
+        expect(body.keys).to eq([:data])
+        expect(body[:data].keys).to eq([:error])
+      end
+    end
   end
 end
